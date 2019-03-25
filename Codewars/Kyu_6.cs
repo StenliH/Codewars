b34b757c70ccc950fc43f080bf445b47d2d799e4
Martin Hlaváč <hlavam12@gmail.com>
2019-03-25 11:52:43 +0100

    Added FindMisingLetter kata

--- Codewars/Kyu_6.cs
+++ Codewars/Kyu_6.cs
@@ -1,9 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Codewars
 {
 	static class Kyu_6
 	{
+		public static char FindMissingLetter(char[] array)
+		{
+			char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
+			bool isUpper = Char.IsUpper(array[0]);
+
+			if (!isUpper)
+			{
+				for (int i = 0; i < array.Length; i++)
+				{
+					array[i] = Char.ToUpper(array[i]);
+				}
+			}
+
+			int index = Array.IndexOf(alphabet, array[0]);
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (array[i] != alphabet[i + index])
+				{
+					char output = alphabet[i + index];
+					return isUpper ? output : Char.ToLower(output);
+				}
+			}
+
+			return ' ';
+		}
+
+		public static void Run_FindMissingLetter()
+		{
+			Console.WriteLine($"Missing letter is: { FindMissingLetter(new char[] { 'O','Q','R','S' }) }");
+			Console.ReadLine();
+		}
+
 		public static string LongestConsec(string[] strarr, int k)
 		{
 			int n = strarr.Length;
